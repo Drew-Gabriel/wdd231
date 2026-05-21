eca48f366ff40f708d8bb33d3ad32b18ddc48f3c
Drew-Gabriel <126345558+Drew-Gabriel@users.noreply.github.com>
2026-05-20 20:57:37 -0700

    Update home.js

--- chamber/scripts/home.js
+++ chamber/scripts/home.js
@@ -32,3 +32,46 @@ fetch("data/members.json")
       spot.appendChild(div);
     });
   });
+// ==========================
+// WEATHER API
+// ==========================
+
+const weatherContainer = document.querySelector("#weather");
+
+async function loadWeather() {
+  try {
+
+    // Benin City coordinates
+    const url =
+    "https://api.open-meteo.com/v1/forecast?latitude=6.34&longitude=5.62&current=temperature_2m&daily=temperature_2m_max,temperature_2m_min&forecast_days=3";
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    weatherContainer.innerHTML = `
+      <p><strong>${data.current.temperature_2m}°C</strong></p>
+
+      <h4>3 Day Forecast</h4>
+
+      <p>Day 1:
+      ${data.daily.temperature_2m_max[0]}° /
+      ${data.daily.temperature_2m_min[0]}°</p>
+
+      <p>Day 2:
+      ${data.daily.temperature_2m_max[1]}° /
+      ${data.daily.temperature_2m_min[1]}°</p>
+
+      <p>Day 3:
+      ${data.daily.temperature_2m_max[2]}° /
+      ${data.daily.temperature_2m_min[2]}°</p>
+    `;
+
+  } catch(error) {
+    weatherContainer.innerHTML =
+    "<p>Weather unavailable</p>";
+
+    console.log(error);
+  }
+}
+
+loadWeather();
