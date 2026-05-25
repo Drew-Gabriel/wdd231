c2795ccbdbf502ba13bcaf48348e43f1b8ad388b
Drew-Gabriel <126345558+Drew-Gabriel@users.noreply.github.com>
2026-05-25 05:20:51 -0700

    Update join.js

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,1 +1,8 @@
+console.log("join.js working");
 
+// Hidden timestamp
+const timestamp = document.getElementById("timestamp");
+
+if (timestamp) {
+    timestamp.value = new Date().toISOString();
+}
